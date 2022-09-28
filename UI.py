a716275c6e088ed534256ae8b9287ed7d116823f
unrealJuanpa <juanpacloud@gmail.com>
2022-09-28 17:48:47 -0600

    ultimos cambios interfaz

--- UI.py
+++ UI.py
@@ -1,5 +1,8 @@
 from tkinter import * 
 from tkinter import filedialog
+import subprocess
+import os
+
 
 window=Tk()
 window.geometry('700x600')
@@ -7,8 +10,27 @@ window.config(bg='#2E424D')
 window.title('Proyecto#1 Compiladores')
 
 def openFile():
+    global subprocess
+
     file = filedialog.askopenfile(title='Abrir',filetypes=(('Archivos de Julia', '*.jl'),('Archivos de Lex','*.l')))
-    listbox.insert(END, file.name)
+
+    os.system('flex archivo.l')
+    os.system('gcc lex.yy.c -L"C:\\MinGW\\msys\\1.0\\lib" -lfl -o archivo.exe')
+
+    ruta_archivo = file.name + ""
+
+    with open(ruta_archivo, 'r') as file:
+        lns = file.readlines()
+
+    for p in lns:
+        listbox.insert(END, p)
+
+    subprocess = subprocess.Popen("archivo.exe <" + ruta_archivo, shell=True, stdout=subprocess.PIPE)
+    subprocess_return = subprocess.stdout.read().decode('utf-8')
+    print(subprocess_return)
+
+    for p in subprocess_return.split('\n'):
+        listbox2.insert(END, p)
 
 #
 listbox=Listbox(window)
